ef3f6c6b1f35d3a0132f0f41581c88817e50801b
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-05-16 00:51:02 +0700

    feat: setup shadcn & add tootlip component

--- src/components/react/ListCard.tsx
+++ src/components/react/ListCard.tsx
@@ -3,16 +3,21 @@ import { projectData } from "./listdata";
 
 export default function ListCard() {
   return (
-    <div className="max-w-[1420px] mx-auto flex justify-center items-center mt-[20px]">
-      {projectData.map((item) => (
-        <Card
-          stack={item.stack}
-          title={item.title}
-          code_url={item.code_url}
-          preview_url={item.preview_url}
-          img_path={item.img.src}
-        />
-      ))}
-    </div>
+    <>
+      <h1 className="my-[30px] text-center text-white font-bold text-[30px]">
+        Fun With API
+      </h1>
+      <div className="max-w-[1420px] mx-auto flex justify-center items-center mt-[20px] gap-8">
+        {projectData.map((item) => (
+          <Card
+            stack={item.stack}
+            title={item.title}
+            code_url={item.code_url}
+            preview_url={item.preview_url}
+            img_path={item.img.src}
+          />
+        ))}
+      </div>
+    </>
   );
 }
--- src/components/react/listdata.tsx
+++ src/components/react/listdata.tsx
@@ -1,8 +1,10 @@
 import MovieList from "../../assets/movie-list.png";
+import GithubUserSearcher from "../../assets/github-user-searcher.png";
 import { GrReactjs } from "react-icons/gr";
 import { SiNextdotjs } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { SiReactquery } from "react-icons/si";
+import { SiShadcnui } from "react-icons/si";
 
 export const projectData = [
   {
@@ -15,6 +17,19 @@ export const projectData = [
       { tech: <SiNextdotjs size={22} className="text-white" /> },
       { tech: <RiTailwindCssFill size={22} className="text-sky-400" /> },
       { tech: <SiReactquery size={22} className="text-red-400" /> },
+    ],
+  },
+  {
+    img: GithubUserSearcher,
+    title: "Github User Searcher",
+    preview_url: "https://simple-movie-list-neon.vercel.app/",
+    code_url: "https://github.com/Nurrohmann/simple-movie-list",
+    stack: [
+      { tech: <GrReactjs size={22} className="text-sky-400" /> },
+      { tech: <SiNextdotjs size={22} className="text-white" /> },
+      { tech: <RiTailwindCssFill size={22} className="text-sky-400" /> },
+      { tech: <SiReactquery size={22} className="text-red-400" /> },
+      { tech: <SiShadcnui size={15} className="text-neutral-200" /> },
     ],
   },
 ];
